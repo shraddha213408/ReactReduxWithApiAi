55b0bcfd286de443444b3cc724885e5f10c04675
shraddha213408 <shraddha.madhogaria@cognizant.com>
2017-04-21 13:34:04 +1000

    Update message.js

--- src/components/message.js
+++ src/components/message.js
@@ -1,9 +1,8 @@
 import React, {Component, PropTypes} from 'react';
-
+import html from 'html-parse-stringify';
 class Message extends Component {
 	render() {
 		const fromMe = this.props.fromMe ? 'from-me' : '';
-
 		return (
 			<div className={`message ${fromMe}`}>
 				<div className="message-body">
